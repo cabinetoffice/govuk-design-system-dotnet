8fe2aea7acf5ab2e61e636274a7c5d849e5b21a6
Dan Corder <dan.corder@ofgem.gov.uk>
2020-01-30 09:20:46 +0000

    feat: Replace custom validated text area with model state version

--- HtmlGenerators/TextAreaHtmlGenerator.cs
+++ HtmlGenerators/TextAreaHtmlGenerator.cs
@@ -11,45 +11,7 @@ namespace GovUkDesignSystem.HtmlGenerators
 {
     internal static class TextAreaHtmlGenerator
     {
-
-        internal static IHtmlContent GenerateHtml<TModel>(
-            IHtmlHelper<TModel> htmlHelper,
-            Expression<Func<TModel, string>> propertyLambdaExpression,
-            int? rows = null,
-            LabelViewModel labelOptions = null,
-            HintViewModel hintOptions = null,
-            FormGroupViewModel formGroupOptions = null
-        )
-            where TModel : GovUkViewModel
-        {
-            PropertyInfo property = ExpressionHelpers.GetPropertyFromExpression(propertyLambdaExpression);
-
-            string propertyName = property.Name;
-
-            TModel model = htmlHelper.ViewData.Model;
-
-            string currentValue = ExtensionHelpers.GetCurrentValue(model, property, propertyLambdaExpression);
-
-
-            var textAreaViewModel = new TextAreaViewModel {
-                Name = $"GovUk_Text_{propertyName}",
-                Id = $"GovUk_{propertyName}",
-                Value = currentValue,
-                Rows = rows,
-                Label = labelOptions,
-                Hint = hintOptions,
-                FormGroup = formGroupOptions
-            };
-
-            if (model.HasErrorFor(property))
-            {
-                textAreaViewModel.ErrorMessage = new ErrorMessageViewModel {Text = model.GetErrorFor(property)};
-            }
-
-            return htmlHelper.Partial("/GovUkDesignSystemComponents/Textarea.cshtml", textAreaViewModel);
-        }
-
-        internal static async Task<IHtmlContent> GenerateHtmlFromModelState<TModel>(
+        internal static async Task<IHtmlContent> GenerateHtml<TModel>(
             IHtmlHelper<TModel> htmlHelper,
             Expression<Func<TModel, string>> propertyExpression,
             int? rows = null,
@@ -99,6 +61,5 @@ namespace GovUkDesignSystem.HtmlGenerators
 
             return await htmlHelper.PartialAsync("/GovUkDesignSystemComponents/Textarea.cshtml", textAreaViewModel);
         }
-
     }
 }
