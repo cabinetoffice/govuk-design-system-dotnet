f0c61f8f552372ea740b416fc85fd5f1e1bea444
Dan Corder <dan.corder@ofgem.gov.uk>
2020-01-30 09:02:15 +0000

    feat: Add standard text area

--- HtmlGenerators/TextAreaHtmlGenerator.cs
+++ HtmlGenerators/TextAreaHtmlGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Threading.Tasks;
 using GovUkDesignSystem.GovUkDesignSystemComponents;
 using GovUkDesignSystem.Helpers;
 using Microsoft.AspNetCore.Html;
@@ -46,6 +47,57 @@ namespace GovUkDesignSystem.HtmlGenerators
             }
 
             return htmlHelper.Partial("/GovUkDesignSystemComponents/Textarea.cshtml", textAreaViewModel);
+        }
+
+        internal static async Task<IHtmlContent> GenerateHtmlFromModelState<TModel>(
+            IHtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, string>> propertyExpression,
+            int? rows = null,
+            LabelViewModel labelOptions = null,
+            HintViewModel hintOptions = null,
+            FormGroupViewModel formGroupOptions = null
+        )
+            where TModel : class
+        {
+            string propertyId = htmlHelper.IdFor(propertyExpression);
+            string propertyName = htmlHelper.NameFor(propertyExpression);
+            htmlHelper.ViewData.ModelState.TryGetValue(propertyName, out var modelStateEntry);
+
+            // Get the value to put in the input from the post data if possible, otherwise use the value in the model
+            string inputValue = null;
+            if (modelStateEntry != null && modelStateEntry.RawValue != null)
+            {
+                inputValue = modelStateEntry.RawValue as string;
+            }
+            else
+            {
+                var modelValue = ExpressionHelpers.GetPropertyValueFromModelAndExpression(htmlHelper.ViewData.Model, propertyExpression);
+                if (modelValue != null)
+                {
+                    inputValue = modelValue.ToString();
+                }
+            }
+
+            //qq:DCC Update label for property
+
+            var textAreaViewModel = new TextAreaViewModel
+            {
+                Name = propertyName,
+                Id = propertyId,
+                Value = inputValue,
+                Rows = rows,
+                Label = labelOptions,
+                Hint = hintOptions,
+                FormGroup = formGroupOptions
+            };
+
+            if (modelStateEntry != null && modelStateEntry.Errors.Count > 0)
+            {
+                // qq:DCC Are we OK with only displaying the first error message here?
+                textAreaViewModel.ErrorMessage = new ErrorMessageViewModel { Text = modelStateEntry.Errors[0].ErrorMessage };
+            }
+
+            return await htmlHelper.PartialAsync("/GovUkDesignSystemComponents/Textarea.cshtml", textAreaViewModel);
         }
 
     }
