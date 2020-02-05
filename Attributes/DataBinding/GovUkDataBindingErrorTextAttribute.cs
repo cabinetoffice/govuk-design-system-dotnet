6aefc9dca9fce74900678a4b5d30ebd070d561bd
Dan Corder <dan.corder@ofgem.gov.uk>
2020-02-05 08:49:35 +0000

    fix: Add AttributeUsage to new attributes

--- Attributes/DataBinding/GovUkDataBindingErrorTextAttribute.cs
+++ Attributes/DataBinding/GovUkDataBindingErrorTextAttribute.cs
@@ -5,6 +5,7 @@ namespace GovUkDesignSystem.Attributes.DataBinding
     /// <summary>
     /// Abstract base class for all attributes that hold data binding error text
     /// </summary>
+    [AttributeUsage(AttributeTargets.Property, Inherited = true)]
     public abstract class GovUkDataBindingErrorTextAttribute : Attribute
     {
     }
