a816ff79b65fdf7a4dd909ff19b1ce9989cd3a48
Waqar Nazir <waqar.nazir@ofgem.gov.uk>
2020-02-13 14:45:00 +0000

    Adding Table component.

---
+++ GovUkDesignSystemComponents/TableGovUkViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GovUkDesignSystem.GovUkDesignSystemComponents
+{
+    public class TableGovUkViewModel
+    {
+
+        /// <summary>
+        ///     Required. List of table rows and cells.
+        /// </summary>
+        public List<TableRowViewModel> Rows { set; get; }
+
+        /// <summary>
+        ///     List of table head cells.
+        /// </summary>
+        public List<TableCellViewModel> Head { set; get; }
+
+        /// <summary>
+        ///     Caption text.
+        /// </summary>
+        public string Caption { get; set; }
+
+        /// <summary>
+        ///     Classes for caption text size. Classes should correspond to the available typography heading classes.
+        /// </summary>
+        public string CaptionClasses { get; set; }
+
+        /// <summary>
+        ///     	If set to true, first cell in table row will be a TH instead of a TD.
+        /// </summary>
+        public bool FirstCellIsHeader { get; set; }
+
+        /// <summary>
+        ///     Classes to add to the table container.
+        /// </summary>
+        public string Classes { get; set; }
+
+        /// <summary>
+        ///     HTML attributes (for example data attributes) to add to the container..
+        /// </summary>
+        public Dictionary<string, string> Attributes { get; set; }
+    }
+}
--- GovUkHtmlHelperExtensions.cs
+++ GovUkHtmlHelperExtensions.cs
@@ -295,5 +295,12 @@ namespace GovUkDesignSystem
                 classes,
                 textInputAppendix);
         }
+
+        public static IHtmlContent GovUkTable(
+            this IHtmlHelper htmlHelper,
+            TableGovUkViewModel tableViewModel)
+        {
+            return htmlHelper.Partial("/GovUkDesignSystemComponents/Table.cshtml", tableViewModel);
+        }
     }
 }
