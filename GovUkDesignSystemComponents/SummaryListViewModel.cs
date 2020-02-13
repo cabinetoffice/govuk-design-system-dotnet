afb0b19dea7eac137c5996d536b0e5eb50e1e951
Waqar Nazir <waqar.nazir@ofgem.gov.uk>
2020-02-13 15:09:37 +0000

    feat: Adding summary list component

---
+++ GovUkDesignSystemComponents/SummaryListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GovUkDesignSystem.GovUkDesignSystemComponents
+{
+    public class SummaryListViewModel
+    {
+        /// <summary>
+        ///     Required. List of row item objects.
+        /// </summary>
+        public List<SummaryListRowViewModel> Rows { set; get; }
+
+        /// <summary>
+        ///     Classes to add to the container.
+        /// </summary>
+        public string Classes { get; set; }
+
+        /// <summary>
+        ///     HTML attributes (for example data attributes) to add to the container.
+        /// </summary>
+        public Dictionary<string, string> Attributes { get; set; }
+    }
+}
