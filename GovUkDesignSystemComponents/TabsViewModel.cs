a906756abfb74faa692b68e932fea5d1bd07773d
Chris Asprou <Chris.Asprou@ofgem.gov.uk>
2020-02-14 13:58:58 +0000

    Changes in pull request review.

--- GovUkDesignSystemComponents/TabsViewModel.cs
+++ GovUkDesignSystemComponents/TabsViewModel.cs
@@ -34,7 +34,7 @@ namespace GovUkDesignSystem.GovUkDesignSystemComponents
         /// <summary>
         ///     List of tab items
         /// </summary>
-        public List<TabsListItemViewModel> TabsList { get; set; }
+        public List<TabsListItemViewModel> Items { get; set; }
     }
 
     // Tab
@@ -55,16 +55,20 @@ namespace GovUkDesignSystem.GovUkDesignSystemComponents
         /// </summary>
         public Dictionary<string, string> Attributes { get; set; }
         
-        public TabsListItemSectionViewModel Section { get; set; }
+        public TabsListItemSectionViewModel Panel { get; set; }
     }
 
     // Section (panel)
     public class TabsListItemSectionViewModel : IHtmlText
     {
-        public string Id { get; set; }
-
+        /// <summary>
+        /// Required. If html is set, this is not required. Text to use within each tab panel. If html is provided, the text argument will be ignored.
+        /// </summary>
         public string Text { get; set; }
 
+        /// <summary>
+        /// Required. If text is set, this is not required. HTML to use within the each tab panel. If html is provided, the text argument will be ignored.
+        /// </summary>
         public Func<object, object> Html { get; set; }
 
         /// <summary>
