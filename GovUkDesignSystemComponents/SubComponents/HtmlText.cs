069b41c604c94b19ed8ef63294d4a803b601e73e
Waqar Nazir <waqar.nazir@ofgem.gov.uk>
2020-02-18 11:16:17 +0000

    Changes after review

---
+++ GovUkDesignSystemComponents/SubComponents/HtmlText.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GovUkDesignSystem.GovUkDesignSystemComponents.SubComponents
+{
+    public class HtmlText : IHtmlText
+    {
+        public Func<object, object> Html { get; set; }
+
+        public string Text { get; set; }
+
+        public HtmlText(Func<object, object> html, string text)
+        {
+            Html = html;
+            Text = text; 
+        }
+    }
+}
