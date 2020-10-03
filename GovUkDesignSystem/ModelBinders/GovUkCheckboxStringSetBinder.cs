e66bfaf12672a4d45901107fd970bbb6de9e3e69
Dan Corder <dan.corder@ofgem.gov.uk>
2020-10-03 11:37:56 +0100

    fix: Add custom model binder to ignore hidden value

---
+++ GovUkDesignSystem/ModelBinders/GovUkCheckboxStringSetBinder.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Microsoft.Extensions.Logging;
+
+namespace GovUkDesignSystem.ModelBinders
+{
+    public class GovUkCheckboxStringSetBinder : CollectionModelBinder<string>
+    {
+        public GovUkCheckboxStringSetBinder(ILoggerFactory loggerFactory) :
+            base(new GovUkCheckboxStringBinder(loggerFactory), loggerFactory)
+        { }
+    }
+}
