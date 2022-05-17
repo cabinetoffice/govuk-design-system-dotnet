ce01a8fa038441a67f2cab12433bf496a279863c
CodCot <codrin.cotarlan@softwire.com>
2022-05-17 16:16:08 +0100

    test: Add tests for all banner states and cookie types

--- GovUkDesignSystem.SnapshotTests/GovUkDesignSystemComponents/CookieBannerTests.cs
+++ GovUkDesignSystem.SnapshotTests/GovUkDesignSystemComponents/CookieBannerTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using GovUkDesignSystem.GovUkDesignSystemComponents;
 using GovUkDesignSystem.SnapshotTests.Helpers;
 using Xunit;
@@ -14,6 +13,7 @@ namespace GovUkDesignSystem.SnapshotTests.GovUkDesignSystemComponents
             {
                 BannerState = BannerState.ShowBanner,
                 CookieType = CookieType.Analytics,
+                AntiForgeryToken = false,
                 ButtonClickAction = "link_after_accept_or_reject",
                 ViewCookiesLink = "link_to_view_cookies",
                 ReturnUrl = "original_url"
@@ -25,6 +25,50 @@ namespace GovUkDesignSystem.SnapshotTests.GovUkDesignSystemComponents
         {
             // Act & Assert
             await VerifyPartial("CookieBanner", DefaultCookieBannerViewModel());
+        }
+        
+        [Fact]
+        public async Task Render_AdditionalCookies()
+        {
+            // Arrange
+            var viewModel = DefaultCookieBannerViewModel();
+            viewModel.CookieType = CookieType.Additional;
+            
+            // Act & Assert
+            await VerifyPartial("CookieBanner", viewModel);
+        }
+        
+        [Fact]
+        public async Task Render_AcceptedBanner()
+        {
+            // Arrange
+            var viewModel = DefaultCookieBannerViewModel();
+            viewModel.BannerState = BannerState.ShowAccepted;
+            
+            // Act & Assert
+            await VerifyPartial("CookieBanner", viewModel);
+        }
+        
+        [Fact]
+        public async Task Render_RejectedBanner()
+        {
+            // Arrange
+            var viewModel = DefaultCookieBannerViewModel();
+            viewModel.BannerState = BannerState.ShowRejected;
+            
+            // Act & Assert
+            await VerifyPartial("CookieBanner", viewModel);
+        }
+        
+        [Fact]
+        public async Task Render_HiddenBanner()
+        {
+            // Arrange
+            var viewModel = DefaultCookieBannerViewModel();
+            viewModel.BannerState = BannerState.Hide;
+            
+            // Act & Assert
+            await VerifyPartial("CookieBanner", viewModel);
         }
     }
 }
