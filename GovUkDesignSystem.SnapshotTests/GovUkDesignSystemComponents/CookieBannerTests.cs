b8de3036fe44d41052c0d83686fe597210388e6f
CodCot <codrin.cotarlan@softwire.com>
2022-05-17 15:53:59 +0100

    test: Add cookieBanner tests file

---
+++ GovUkDesignSystem.SnapshotTests/GovUkDesignSystemComponents/CookieBannerTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GovUkDesignSystem.GovUkDesignSystemComponents;
+using GovUkDesignSystem.SnapshotTests.Helpers;
+using Xunit;
+
+namespace GovUkDesignSystem.SnapshotTests.GovUkDesignSystemComponents
+{
+    public class CookieBannerTests : SnapshotTestBase
+    {
+        private CookieBannerViewModel DefaultCookieBannerViewModel()
+        {
+            return new CookieBannerViewModel
+            {
+                BannerState = BannerState.ShowBanner,
+                CookieType = CookieType.Analytics,
+                ButtonClickAction = "link_after_accept_or_reject",
+                ViewCookiesLink = "link_to_view_cookies",
+                ReturnUrl = "original_url"
+            };
+        }
+
+        [Fact]
+        public async Task Render_AllValues()
+        {
+            // Act & Assert
+            await VerifyPartial("CookieBanner", DefaultCookieBannerViewModel());
+        }
+    }
+}
