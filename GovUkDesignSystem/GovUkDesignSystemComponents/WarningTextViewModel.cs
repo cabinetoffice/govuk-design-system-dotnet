6dc556a31bae439c6d5f9cb226a2edbb04c7b515
CodCot <codrin.cotarlan@softwire.com>
2022-05-20 13:51:04 +0100

    feat: Add warning text component and a test for it

---
+++ GovUkDesignSystem/GovUkDesignSystemComponents/WarningTextViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GovUkDesignSystem.GovUkDesignSystemComponents;
+
+public class WarningTextViewModel
+{
+    /// <summary>
+    ///     Required. The fallback text for the icon
+    /// </summary>
+    public string IconFallbackText { get; set; }
+    
+    /// <summary>
+    ///     Required. Text to use within the warning text component
+    /// </summary>
+    public string Text { get; set; }
+}
