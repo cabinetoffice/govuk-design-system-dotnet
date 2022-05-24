742bacacd0d800f91a27fc421b0c2ba1bc6e4d76
CodCot <codrin.cotarlan@softwire.com>
2022-05-24 17:10:37 +0100

    feat: Add all fields to the Warning text component

--- GovUkDesignSystem/GovUkDesignSystemComponents/WarningTextViewModel.cs
+++ GovUkDesignSystem/GovUkDesignSystemComponents/WarningTextViewModel.cs
@@ -1,14 +1,38 @@
-﻿namespace GovUkDesignSystem.GovUkDesignSystemComponents;
+﻿using System;
+using System.Collections.Generic;
+using GovUkDesignSystem.GovUkDesignSystemComponents.SubComponents;
 
-public class WarningTextViewModel
+namespace GovUkDesignSystem.GovUkDesignSystemComponents;
+
+public class WarningTextViewModel: IHtmlText
 {
+    
     /// <summary>
-    ///     Required. The fallback text for the icon
+    ///     Required. If `html` is set, this is not required.
+    ///     Text to use within the warning text component.
+    ///     If `html` is provided, the `text` argument will be ignored.
+    /// </summary>
+    public string Text { get; set; }
+
+    /// <summary>
+    ///     Required. If `text` is set, this is not required.
+    ///     HTML to use within the warning text component.
+    ///     If `html` is provided, the `text` argument will be ignored.
+    /// </summary>
+    public Func<object, object> Html { get; set; }
+    
+    /// <summary>
+    ///     Required. The fallback text for the icon.
     /// </summary>
     public string IconFallbackText { get; set; }
     
     /// <summary>
-    ///     Required. Text to use within the warning text component
+    ///     Classes to add to the warning text.
     /// </summary>
-    public string Text { get; set; }
+    public string Classes { get; set; }
+    
+    /// <summary>
+    ///     HTML attributes (for example data attributes) to add to the warning text.
+    /// </summary>
+    public Dictionary<string, string> Attributes { get; set; }
 }
