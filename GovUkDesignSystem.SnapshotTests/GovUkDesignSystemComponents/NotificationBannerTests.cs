8a34336a0769bf539f531b5d66d15af510eb7b28
CodCot <codrin.cotarlan@softwire.com>
2022-06-21 17:45:55 +0100

    feat: Add notification banner component

---
+++ GovUkDesignSystem.SnapshotTests/GovUkDesignSystemComponents/NotificationBannerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GovUkDesignSystem.GovUkDesignSystemComponents;
+using GovUkDesignSystem.GovUkDesignSystemComponents.SubComponents;
+using GovUkDesignSystem.SnapshotTests.Helpers;
+using Xunit;
+
+namespace GovUkDesignSystem.SnapshotTests.GovUkDesignSystemComponents
+{
+    public class CNotificationBannerTests : SnapshotTestBase
+    {
+        private NotificationBannerViewModel DefaultNotificationBannerViewModel()
+        {
+            return new NotificationBannerViewModel
+            {
+                Text = "Banner text",
+                TitleText = "Title text",
+                TitleHeadingLevel = "3",
+                Type = "success",
+                TitleId = "title-id",
+                DisableAutoFocus = true,
+                Classes = "custom-class"
+            };
+        }
+
+        [Fact]
+        public async Task Render_AllValues()
+        {
+            // Act & Assert
+            await VerifyPartial("NotificationBanner", DefaultNotificationBannerViewModel());
+        }
+    }
+}
