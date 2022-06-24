3d1e8b26e468a7bddf7bc1237b3c21637cf4bd75
Dan Corder <dan.corder@softwire.com>
2022-06-24 10:48:48 +0100

    refactor: remove unused usings

--- GovUkDesignSystem.SnapshotTests/GovUkDesignSystemComponents/FileUploadTests.cs
+++ GovUkDesignSystem.SnapshotTests/GovUkDesignSystemComponents/FileUploadTests.cs
@@ -1,9 +1,7 @@
 using System.Collections.Generic;
-using System.IO;
 using System.Threading.Tasks;
 using GovUkDesignSystem.GovUkDesignSystemComponents;
 using GovUkDesignSystem.SnapshotTests.Helpers;
-using Microsoft.AspNetCore.Http;
 using Xunit;
 
 namespace GovUkDesignSystem.SnapshotTests.GovUkDesignSystemComponents
