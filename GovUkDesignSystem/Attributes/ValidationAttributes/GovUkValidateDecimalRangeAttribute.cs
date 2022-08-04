0e5ab4f7304fd08dc7c4f13d496ce9afe25cb49a
CodCot <codrin.cotarlan@softwire.com>
2022-08-04 13:52:34 +0100

    feat: Add custom eror message for decimal range validation attribute

--- GovUkDesignSystem/Attributes/ValidationAttributes/GovUkValidateDecimalRangeAttribute.cs
+++ GovUkDesignSystem/Attributes/ValidationAttributes/GovUkValidateDecimalRangeAttribute.cs
@@ -5,10 +5,10 @@ namespace GovUkDesignSystem.Attributes.ValidationAttributes
     public class GovUkValidateDecimalRangeAttribute : RangeAttribute
     {
         // Attributes do not allow decimals as parameters, but allow for their string representation
-        public GovUkValidateDecimalRangeAttribute(string propertyName, double minimum, double maximum)
+        public GovUkValidateDecimalRangeAttribute(string propertyName, double minimum, double maximum, string customErrorMessage = null)
             : base(typeof(decimal), minimum.ToString(), maximum.ToString())
         {
-            ErrorMessage = $"{propertyName} must be between {minimum} and {maximum}";
+            ErrorMessage = customErrorMessage ?? $"{propertyName} must be between {minimum} and {maximum}";
         }
     }
 }
