bae2d849b8c41596546700b56ecab2f8527b5b14
CodCot <codrin.cotarlan@softwire.com>
2022-09-22 12:39:36 +0100

    feat: Add pagination component

---
+++ GovUkDesignSystem/GovUkDesignSystemComponents/PaginationViewModel.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+
+namespace GovUkDesignSystem.GovUkDesignSystemComponents;
+
+public class PaginationViewModel
+{
+    /// <summary>
+    /// The array of link objects.
+    /// </summary>
+    public List<PaginationItemViewModel> Items { get; set; }
+
+    /// <summary>
+    /// A link to the previous page, if there is a previous page.
+    /// </summary>
+    public PaginationPreviousViewModel Previous { get; set; }
+    
+    /// <summary>
+    /// A link to the next page, if there is a next page.
+    /// </summary>
+    public PaginationNextViewModel Next { get; set; }
+    
+    /// <summary>
+    /// The label for the navigation landmark that wraps the pagination. Defaults to 'results'.
+    /// </summary>
+    public string LandmarkLabel { get; set; }
+    
+    /// <summary>
+    /// The classes you want to add to the pagination nav parent.
+    /// </summary>
+    public string Classes { get; set; }
+    
+    /// <summary>
+    /// The HTML attributes (for example, data attributes) you want to add to the pagination nav parent.
+    /// </summary>
+    public Dictionary<string, string> Attributes { get; set; }
+}
+
+public class PaginationItemViewModel
+{
+    /// <summary>
+    /// The pagination item text - usually a page number.
+    /// </summary>
+    public string Number { get; set; }
+    
+    /// <summary>
+    /// The visually hidden label (for the pagination item) which will be applied to an aria-label and announced
+    /// by screen readers on the pagination item link. Should include page number.
+    /// </summary>
+    public string VisuallyHiddenText { get; set; }
+    
+    /// <summary>
+    /// <b>Required</b>. The link's URL.
+    /// </summary>
+    public string Href { get; set; }
+    
+    /// <summary>
+    /// Set to true to indicate the current page the user is on.
+    /// </summary>
+    public bool Current { get; set; }
+    
+    /// <summary>
+    /// Use this option if you want to specify an ellipsis at a given point between numbers.
+    /// If you set this option as true, any other options for the item are ignored.
+    /// </summary>
+    public bool Ellipsis { get; set; }
+    
+    /// <summary>
+    /// The HTML attributes (for example, data attributes) you want to add to the anchor.
+    /// </summary>
+    public Dictionary<string, string> Attributes { get; set; }
+}
+
+public class PaginationPreviousViewModel
+{
+    /// <summary>
+    /// The link text to the previous page. Defaults to 'Previous page', where 'page' is visually hidden.
+    /// </summary>
+    public string Text { get; set; }
+    
+    /// <summary>
+    /// The optional label that goes underneath the link to the previous page,
+    /// providing further context for the user about where the link goes.
+    /// </summary>
+    public string LabelText { get; set; }
+    
+    /// <summary>
+    /// <b>Required</b>. The previous page's URL.
+    /// </summary>
+    public string Href { get; set; }
+    
+    /// <summary>
+    /// The HTML attributes (for example, data attributes) you want to add to the anchor.
+    /// </summary>
+    public Dictionary<string, string> Attributes { get; set; }
+}
+
+public class PaginationNextViewModel
+{
+    /// <summary>
+    /// The link text to the next page. Defaults to 'Next page', where 'page' is visually hidden.
+    /// </summary>
+    public string Text { get; set; }
+    
+    /// <summary>
+    /// The optional label that goes underneath the link to the next page,
+    /// providing further context for the user about where the link goes.
+    /// </summary>
+    public string LabelText { get; set; }
+    
+    /// <summary>
+    /// <b>Required</b>. The next page's URL.
+    /// </summary>
+    public string Href { get; set; }
+    
+    /// <summary>
+    /// The HTML attributes (for example, data attributes) you want to add to the anchor.
+    /// </summary>
+    public Dictionary<string, string> Attributes { get; set; }
+}
